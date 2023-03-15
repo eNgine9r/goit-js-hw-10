1624e4a35a95cfcf041b2e1099d6e867bfb9c30b
Serhii Tymchuk <92324923+eNgine9r@users.noreply.github.com>
2023-03-15 21:26:39 +0200

    update task 10 | done

--- src/index.js
+++ src/index.js
@@ -1,97 +1,71 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-    input: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
 
-refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function onSearch(e) {
-  const searchQuery = e.target.value.trim();
-  if (searchQuery === '') {
+function onInput() {
+  const country = refs.input.value.trim();
+  if (!country) {
     clearMarkup();
     return;
   }
-  fetchCountries(searchQuery)
-    .then(handleError)
-    .then(onFetch)
-    .catch(fetchError);
-}
 
-function onFetch(data) {
-  if (data.length >= 10) {
-    Notify.info(
-      'Too many matches found. Please enter a more specific name.',
-      notificationOptions
+  fetchCountries(country)
+    .then(data => {
+      if (data.length >= 10) {
+        clearMarkup();
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        clearMarkup();
+        createCountryName(data);
+      }
+      if (data.length === 1) {
+        clearMarkup();
+        createCountryData(data);
+        createCountryName(data);
+      }
+    })
+    .catch(error =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
     );
-  }  else if (data.length > 1 && data.length < 10) {
-    showFlag(data);
-  } else if (data.length === 1) {
-    showInfo(data);
-  }
 }
 
-function handleError(data) {
-  if (data.status === 404) {
-    throw new Error(data.message);
-  }
-  return data;
-}
-
-function fetchError(error) {
-  console.log(error.message);
-  clearMarkup();
-  Notify.failure(
-    'Oops, there is no country with that name',
-    notificationOptions
-  );
-}
-
-function showFlag(countries) {
-  clearMarkup();
-  const markup = countries
-    .map(
-      country =>
-        `<li class="country"><img src='${country.flags.svg}' width=30 height=20 alt="${country.flags.alt}"><p class="name">${country.name.common}</p></li>`
-    )
+function createCountryName(arrayCountryName) {
+  const countryCardMarkup = arrayCountryName
+    .map(({ name, flags }) => {
+      return `<li class = "country-list__item"><img src="${flags.svg}" alt="${name.common}" ><span class = "country-list__name">${name.official}</span></li>`;
+    })
     .join('');
-  refs.countriesList.insertAdjacentHTML('afterbegin', markup);
+  refs.countryList.innerHTML = countryCardMarkup;
 }
 
-function showInfo(country) {
-  clearMarkup();
-  const languages = [];
-  Object.entries(country[0].languages).forEach(([key, value]) => {
-    languages.push(value);
-  });
-
-  const markup = `<ul class="country-full">
-        <li class="country"><img src='${
-          country[0].flags.svg
-        }' width=30 height=20 alt="${country[0].flags.alt}">
-            <p class="name">${country[0].name.common}</p>
-        </li>
-        <li class="country"><span class="name">Capital:</span> ${
-          country[0].capital
-        }</li>
-        <li class="country"><span class="name">Population:</span> ${
-          country[0].population
-        }</li>
-        <li class="country"><span class="name">Languages:</span> ${languages.join(
-          ', '
-        )}</li>
-    </ul>`;
-  refs.countryData.innerHTML = markup;
+function createCountryData(countryName) {
+  const countryCardMarkup = countryName.map(
+    ({ capital, population, languages }) => {
+      return `<p class = "country-info__data"><b>Capital:</b> ${capital}</p>
+      <p class = "country-info__data"><b>Population:</b> ${population}</p>
+      <p class = "country-info__data"><b>Languages:</b> ${Object.values(
+        languages
+      ).join(', ')}</p>`;
+    }
+  );
+  refs.countryInfo.innerHTML = countryCardMarkup;
 }
 
 function clearMarkup() {
-  refs.countriesList.innerHTML = '';
-  refs.countryData.innerHTML = '';
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
